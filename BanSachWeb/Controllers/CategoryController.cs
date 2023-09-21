7b86aef719e13ad2bf216e16490939ef432d8cf6
Nhat Hoang <Nhat Hoang@DESKTOP-F49TNCC>
2023-09-21 13:49:52 +0700

    Add project files.

---
+++ BanSachWeb/Controllers/CategoryController.cs
@@ -0,0 +1,119 @@
+﻿using BanSachWeb.Data;
+using BanSachWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BanSachWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name==obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The name must not same displayoder");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["Sucess"] = "Category Create Sucessfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id ==0) 
+            {
+                return NotFound();
+            }
+            var categoryfromDb = _db.Categories.Find(id);
+            //var categoryfromDbFrist = _db.Categories.FirstOrDefault(c => c.Id == id);
+            //var categoryfromDbFristSingle = _db.Categories.SingleOrDefault(c => c.Id == id);
+
+            if (categoryfromDb == null) 
+            {
+                return NotFound();
+            }
+            return View(categoryfromDb);
+        }
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The name must not same displayoder");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["Sucess"] = "Category Update Sucessfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryfromDb = _db.Categories.Find(id);
+            //var categoryfromDbFrist = _db.Categories.FirstOrDefault(c => c.Id == id);
+            //var categoryfromDbFristSingle = _db.Categories.SingleOrDefault(c => c.Id == id);
+
+            if (categoryfromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryfromDb);
+        }
+        //post
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            //var categoryfromDbFrist = _db.Categories.FirstOrDefault(c => c.Id == id);
+            //var categoryfromDbFristSingle = _db.Categories.SingleOrDefault(c => c.Id == id);
+            if (obj.Name == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                TempData["Sucess"] = "Category Dalete Sucessfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+    }
+}
